1aeffb632bc0284020d1bdbe6ce039bb2c2cfada
Hearty Oh <heartyoh@hatiolab.com>
2019-04-29 00:47:42 +0800

    Initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,40 @@
+import { html } from 'lit-html'
+
+import { store } from '@things-factory/shell'
+import { i18next } from '@things-factory/i18n-base'
+import { showSnackbar } from '@things-factory/layout-base'
+import { ADD_MORENDA } from '@things-factory/more-base'
+
+export default function bootstrap() {
+  /* add language changed event handler */
+  i18next.on('languageChanged', e => {
+    store.dispatch(
+      showSnackbar(
+        i18next.t('text.you.are.now.in', {
+          state: {
+            text: i18next.t('text.current language')
+          }
+        })
+      )
+    )
+  })
+
+  /* add language setting morenda - should be changed to setting */
+  store.dispatch({
+    type: ADD_MORENDA,
+    morenda: {
+      icon: 'language.png',
+      name: 'set language',
+      template: html`
+        <select
+          @change="${e => {
+            i18next.changeLanguage(e.target.value)
+          }}"
+        >
+          <option value="en-US" ?selected=${i18next.language == 'en-US'}>English</option>
+          <option value="ko-KR" ?selected=${i18next.language == 'ko-KR'}>한국어</option>
+        </select>
+      `
+    }
+  })
+}
