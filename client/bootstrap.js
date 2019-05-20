e5240b09e1e20e61f01bda454902da2f4a5bf8bf
Hearty Oh <heartyoh@hatiolab.com>
2019-05-20 16:24:54 +0900

    v0.1.3

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -10,7 +10,7 @@ export default function bootstrap() {
     document.dispatchEvent(
       new CustomEvent('notify', {
         detail: {
-          type: result ? 'info' : 'error',
+          type: 'info',
           message: i18next.t('text.you.are.now.in', {
             state: {
               text: i18next.t('text.current language')
